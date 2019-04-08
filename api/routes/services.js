56be956ff7d4ddd027b9f3f5c0677ba746b36bd4
ritwizsinha <ritwizsinha0@gmail.com>
2019-04-08 17:34:31 +0530

    added the delete fro service id

--- api/routes/services.js
+++ api/routes/services.js
@@ -10,3 +10,28 @@ const checkAuth = require('../middleware/auth');
 const Services = require('../models/service');
 
 module.exports = router;
+
+router.delete('/:serviceId', checkAuth, (req, res) => {
+    const id = req.params.serviceId;
+    const { isVendor, isAdmin } = req.user
+    if (isVendor || isAdmin) {
+        Service.remove({ vendorId: id }, (err) => {
+            if (!err) {
+                return res.status(200).json({
+                    message: "Successfully Removed"
+                })
+            }
+            else {
+                res.status(500).json({
+                    error: err
+                })
+            }
+        })
+    }
+    else {
+        res.status(500).json({
+            message: "Unauthorized Access"
+        })
+    }
+
+})
