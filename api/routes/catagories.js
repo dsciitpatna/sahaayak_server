3c22b5c68f61173179f5d2884c7507a0a5204bda
DrSamosa <vsubhang4@gmail.com>
2019-04-08 18:05:04 +0530

    Added patch and post request for category

--- api/routes/catagories.js
+++ api/routes/catagories.js
@@ -9,4 +9,76 @@ const checkAuth = require('../middleware/auth');
 
 const Catagories = require('../models/catagory');
 
+router.post('/',(req,res)=>{
+    const {name}=req.body;
+
+    let validator = new v(req.body, {
+        name: 'required|minLength:7'
+    });
+
+    validator.check().then(function(matched){
+        if(!matched)
+        {
+            res.status(422).json({msg:validator.errors})
+        }
+        else{
+            const newCategory = new Catagories({
+                name
+            })
+
+            Catagories.findOne({name})
+                      .then(category=>{
+                        if (category) return res.status(400).json({ msg: 'Category Already exists' })  
+
+                        newCategory
+                            .save()
+                            .then(category=>{
+                                return res.json({
+                                    category: {
+                                        name: category.name
+                                    }
+                                })
+                            })
+                      })
+        }
+    })
+
+})
+
+
+router.patch('/:catagoryId',(req,res)=>{
+    const id=req.params.id;
+
+    let validator = new v(req.body, {
+        name: 'required|minLength:7'
+    });
+
+    validator.check().then(function (matched) {
+        if (!matched) {
+            res.status(422).json({ msg: validator.errors });
+        } else {
+            Catagories.findById(id)
+            .exec()
+            .then(category => {
+                if (category) {
+                    Catagories.findByIdAndUpdate(id,req.body,{new:true}).then((updatedCategory)=>{
+                        res.status(200).json(updatedCategory);
+                    });
+                } else {
+                    res.status(404).json({
+                        message: "No entry found for given ID"
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                })
+            })
+        }
+    });
+
+})
+
 module.exports = router;
