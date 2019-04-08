8a6387ddc43ed7a35b663f716803bd5448f2cb7d
Ashwani Yadav <ashyadavash@gmail.com>
2019-04-08 08:06:27 +0530

    Add Get request /:serviceId

--- api/routes/services.js
+++ api/routes/services.js
@@ -83,4 +83,29 @@ router.post("/", checkAuth, (req, res, next) => {
     
 });
 
+// Get a service by ID  (access: all)
+router.get('/:serviceId',(req, res) => {
+    const id = req.params.serviceId;
+    Service.findById(id)
+        .exec()
+        .then(service => {
+            //console.log(user);
+            if (service) {
+                res.status(200).json({
+                    service: service
+                })
+            } else {
+                res.status(404).json({
+                    message: "No entry found for given ID"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
 module.exports = router;
