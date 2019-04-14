ae23ea84de8e2722f8fa4a045f609100eb79ce38
ritwizsinha <ritwizsinha0@gmail.com>
2019-04-14 13:02:09 +0530

    Api for updating a service

--- api/routes/services.js
+++ api/routes/services.js
@@ -54,11 +54,11 @@ router.post("/", checkAuth, (req, res, next) => {
                 detail,
                 rating
             });
-          
-            Service.findOne({ vendor,name })
+
+            Service.findOne({ vendor, name })
                 .then(service => {
                     if (service) return res.status(400).json({ msg: 'Vendor service already exists' });
-        
+
                     if (req.user.isVendor) {
                         newService
                             .save()
@@ -67,7 +67,7 @@ router.post("/", checkAuth, (req, res, next) => {
                                     service: service
                                 })
                             })
-                        } else {
+                    } else {
                         res.status(401).json({
                             message: "Unautherized access"
                         })
@@ -81,11 +81,11 @@ router.post("/", checkAuth, (req, res, next) => {
                 })
         }
     });
-    
+
 });
 
 // Get a service by ID  (access: all)
-router.get('/:serviceId',(req, res) => {
+router.get('/:serviceId', (req, res) => {
     const id = req.params.serviceId;
     Service.findById(id)
         .populate('vendor', '_id name')
@@ -109,4 +109,46 @@ router.get('/:serviceId',(req, res) => {
         })
 })
 
+// Api for Updating a service
+router.patch('/:serviceId', checkAuth, (req, res) => {
+    if (req.user.isVendor) {
+        const serviceId = req.params.serviceId;
+        const { name, detail, rating } = req.body;
+        // Validation
+        let validator = new v(req.body, {
+            name: 'required|minLength:5'
+        });
+
+        validator.check().then(function (matched) {
+            if (!matched) {
+                res.status(422).json({ msg: validator.errors });
+            }
+            else {
+                Services.findById(serviceId)
+                    .exec()
+                    .then(service => {
+                        if (service) {
+                            Services.findByIdAndUpdate(serviceId, { name, detail, rating }, { new: true }).then((updatedservice) => {
+                                res.status(200).json(updatedservice);
+                            });
+                        } else {
+                            res.status(404).json({
+                                message: "No entry found for given ID"
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err
+                        })
+                    })
+            }
+        })
+    }
+    else {
+        res.status(401).json({
+            message: "Unathorized Session"
+        })
+    }
+})
 module.exports = router;
