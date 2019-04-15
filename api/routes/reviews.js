fd28add57d7d8595cded8f1afaffdcf1cedc945b
ritwizsinha <ritwizsinha0@gmail.com>
2019-04-15 15:44:03 +0530

    Get all reviews for a particular service

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -10,3 +10,29 @@ const checkAuth = require('../middleware/auth');
 const Reviews = require('../models/review');
 
 module.exports = router;
+
+
+
+// Api for getting all reviews for a particular service Id
+
+router.get('/:serviceId', checkAuth, (req, res) => {
+    const serviceId = req.params.serviceId
+    Reviews.find({ service: serviceId })
+        .exec()
+        .then(reviews => {
+            if (reviews.length === 0) {
+                return res.status(404).json({
+                    message: "No reviews found"
+                })
+            }
+            res.status(200).json({
+                reviews: reviews
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+
+})
