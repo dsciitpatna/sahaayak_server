9f13aacb81fc95b0efa469a1a6adfe0eabefc6ee
VatsalSin <33460761+VatsalSin@users.noreply.github.com>
2019-05-19 23:03:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const PORT = process.env.port || 5000;
 
 const server = http.createServer(app);
-server.set('port', process.env.PORT || 5000);
-server.listen(server.get('port'), function () {
+
+server.listen(PORT, function () {
 	console.log('Express server started on port', server.get('port'));
 });
