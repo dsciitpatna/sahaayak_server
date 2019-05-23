8c3e9218c2a4934e81e07ea18e09d3c2510c54ba
Harsh <29hj29@gmail.com>
2019-05-23 12:01:07 +0530

    First node app deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const WebSocketServer = require("ws").Server
 const http = require('http');
 const app = require('./app');
 
@@ -8,3 +9,19 @@ const server = http.createServer(app);
 server.listen(PORT, function () {
 	console.log('Express server started on port', PORT);
 });
+
+let wss = new WebSocketServer({ server: server })
+console.log("websocket server created")
+
+wss.on("connection", function (ws) {
+	let id = setInterval(function () {
+		ws.send(JSON.stringify(new Date()), function () { })
+	}, 1000)
+
+	console.log("websocket connection open")
+
+	ws.on("close", function () {
+		console.log("websocket connection close")
+		clearInterval(id)
+	})
+})
